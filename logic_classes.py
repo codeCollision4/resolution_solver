969f110f65278b8906bfdc56993b82ac523b7a29
codeCollision4 <79557266+codeCollision4@users.noreply.github.com>
2022-04-05 00:18:24 -0500

    Input complete #3

--- logic_classes.py
+++ logic_classes.py
@@ -12,11 +12,29 @@ class Literal():
         # Char used for literal    
         self.atom = lit
 
+    def __str__(self):
+        return '~' + self.atom if self.negated else self.atom
+        
+
 # Holds literals that are connected by OR 
 class Clause():
     def __init__(self, line, idx):
         self.index = idx # Number in KB
         self.literals = line.split() # Getting each literal from line
+
+
+
+
+
+
+
+
+
+
+
+
+    def __str__(self):
+        return str(self.literals)
 
         
 
--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from pathlib import Path
 from logic_classes import Clause
 
 def main():
-
-    # Command Line Parser
+    #Command Line Parser
     parser = argparse.ArgumentParser()
     # Positional Arguments
     parser.add_argument("input",
                         help="Path to a .kb file that holds list of clauses",
                         type=Path
                         )
-    
+
     # Parse input
     args = parser.parse_args()
 
@@ -26,10 +25,14 @@ def main():
 
         # Getting test clause from end of list
         test_clause = kb.pop()
-    
-        print(kb)
+
+        for c in kb:
+            print(c)
         print(test_clause)
-                       
+                        
     else:
         print("Please provide a .kb file that exists. Make sure you are not just providing a directory.")
         exit()
+
+if __name__ == '__main__':
+    main()
